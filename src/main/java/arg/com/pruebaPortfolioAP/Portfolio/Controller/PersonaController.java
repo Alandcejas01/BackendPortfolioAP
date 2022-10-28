ac97437cfb630af0d51f49429d9f6c0dec3fe598
Alan Cejas <cejasalandavid01@gmail.com>
2022-10-27 22:29:54 -0300

    arreglo de persona controller

--- src/main/java/arg/com/pruebaPortfolioAP/Portfolio/Controller/PersonaController.java
+++ src/main/java/arg/com/pruebaPortfolioAP/Portfolio/Controller/PersonaController.java
@@ -44,7 +44,6 @@ public class PersonaController {
         return new ResponseEntity(new Mensaje("Persona eliminada"), HttpStatus.OK);
     }
 
-    /*
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody DtoPersona dtoPersona){
         if(StringUtils.isBlank(dtoPersona.getNombre()))
@@ -56,15 +55,14 @@ public class PersonaController {
         if(StringUtils.isBlank(dtoPersona.getDescripcion()))
             return new ResponseEntity(new Mensaje("La imagen de la persona es obligatoria"), HttpStatus.BAD_REQUEST);
 
-        if(personaService.existsByNombre(dtopersona.getNombre()))
+        if(personaService.existsByNombre(dtoPersona.getNombre()))
             return new ResponseEntity(new Mensaje("Esa persona ya existe"), HttpStatus.BAD_REQUEST);
-        Persona persona = new Persona(dtopersona.getNombre(), dtopersona.getApellido(),
-                dtopersona.getDescripcion(), dtopersona.getImg());
+        Persona persona = new Persona(dtoPersona.getNombre(), dtoPersona.getApellido(), dtoPersona.getCargo(),
+                dtoPersona.getDescripcion(), dtoPersona.getImg());
         personaService.save(persona);
 
         return new ResponseEntity(new Mensaje("Persona agregada"), HttpStatus.OK);
     }
-    */
 
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoPersona dtoPersona){
